ceff7a0ec7f2fdc1a915cae5600c8718a1729fae
Sergey Slobodenyuk <sergn75@gmail.com>
2023-02-17 11:30:56 +0100

    Initial Commit

---
+++ GitHub Watcher/GitHub_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_WatcherApp.swift
+//  GitHub Watcher
+//
+//  Created by Sergey Slobodenyuk on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
